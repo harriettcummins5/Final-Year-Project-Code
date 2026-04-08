5106283eeea01063f4f752d90a924343e6d68e8b
harriettcummins5 <harriett.cummins2@mail.dcu.ie>
2026-04-08 13:52:34 +0100

    Add files via upload

---
+++ analysis.py
@@ -0,0 +1,26 @@
+from fermipy.gtanalysis import GTAnalysis
+
+source = "4FGL J1713.5-3945e"
+model = {"SpatialModel": "PointSource", "Index": 2.0, "SpectrumType": "PowerLaw"}
+
+gta = GTAnalysis("config.yaml", logging={"verbosity": 3})
+gta.setup()
+
+gta.load_roi("/home/oslaf/hcummins/high_energy_analysis/HEA_fixed/SMC_data2_binsz/2.1-roi_after_fit.npy")
+
+gta.free_source(source, free=True)
+gta.fit(min_fit_quality=3, optimizer="NEWMINUIT")
+print(gta.roi[source])
+
+gta.write_roi("bin06_after_fit")
+
+gta.delete_source(source)
+
+gta.residmap("bin06_removed_source", model=model, make_plots=True)
+gta.tsmap("bin06_removed_source", model=model, make_plots=True, multithread=False)
+
+gta.residmap("bin06_removed_source_below100GeV", model=model, make_plots=True, loge_bounds=[None, 5])
+gta.tsmap("bin06_removed_source_below100GeV", model=model, make_plots=True, loge_bounds=[None, 5], multithread=False)
+
+gta.residmap("bin06_removed_source_above100GeV", model=model, make_plots=True, loge_bounds=[5, None])
+gta.tsmap("bin06_removed_source_above100GeV", model=model, make_plots=True, loge_bounds=[5, None], multithread=False)
